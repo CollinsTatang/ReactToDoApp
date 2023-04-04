af5f29530f7c6b048901cd5682a6dce3d6e6f857
Collins Tatang <makungongcollins@gmail.com>
2023-04-04 17:14:25 +0100

    Add input field and button

--- src/App.js
+++ src/App.js
@@ -3,19 +3,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>React Todo App</h1>
+
+     <div className='input-wrapper'>
+      <input type="text" name="todo" placeholder='Create a new todo' />
+      <button className='add-button'>Add</button>
+     </div>
     </div>
   );
 }
