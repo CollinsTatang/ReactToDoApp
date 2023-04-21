b3d2b19a690654834e9b2fdaac57702be37797df
Collins Tatang <makungongcollins@gmail.com>
2023-04-21 14:50:24 +0100

    Update components

--- src/conponents/SubmitForm/SubmitForm.js
+++ src/conponents/SubmitForm/SubmitForm.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './SubmitForm.css';
 
-const SubmitForm = () => (
-  <div className="form">
-    <form>
-      <input className="input" placeholder="Add Task" />
-      <button className="add-button" type="submit">Add</button>
-    </form>
-  </div>
-);
+function SubmitForm() {
+  return (
+    <div className="form">
+      <form>
+        <input className="input" placeholder="Add Task" />
+        <button className="add-button" type="submit">Add</button>
+      </form>
+    </div>
+  );
+}
 export default SubmitForm;
--- src/conponents/Task/Task.js
+++ src/conponents/Task/Task.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import './Task.css';
 
-const Task = () => (
-  <div className="task-container">
-    <div className="task-container-background">
-      <div className="task-term-container">
-        <h2 className="task-term">new task</h2>
-      </div>
-      <div className="buttons-container">
-        <button type="submit" className="done-button">v</button>
-        <button type="submit" className="delete-button">x</button>
+function Task() {
+  return (
+    <div className="task-container">
+      <div className="task-container-background">
+        <div className="task-term-container">
+          <h2 className="task-term">task</h2>
+        </div>
+        <div className="buttons-container">
+          <button type="submit" className="done-button">v</button>
+          <button type="submit" className="delete-button">x</button>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+}
 export default Task;
