3da80fd71de5a457024c9ba1ff4549851087656e
Collins Tatang <makungongcollins@gmail.com>
2023-09-09 18:46:14 +0100

    Add form state

--- src/conponents/SubmitForm/SubmitForm.js
+++ src/conponents/SubmitForm/SubmitForm.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import { useState } from 'react';
+import PropTypes from 'prop-types';
 import './SubmitForm.css';
 
-function SubmitForm() {
+function SubmitForm({ addTodoItem }) {
+  const [title, setTitle] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleChange = (e) => {
+    setTitle(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (title.trim()) {
+      addTodoItem(title);
+      setTitle('');
+      setMessage('Successfully Added');
+    } else {
+      setMessage('Please add item.');
+    }
+  };
   return (
     <div className="form">
-      <form>
-        <input className="input" placeholder="Add Task" />
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={title} onChange={handleChange} className="input" placeholder="Add Task" />
         <button className="add-button" type="submit">Add</button>
       </form>
+      <span>{message}</span>
     </div>
   );
 }
+SubmitForm.propTypes = {
+  addTodoItem: PropTypes.node.isRequired,
+};
 export default SubmitForm;
--- src/conponents/TasksLogic.js
+++ src/conponents/TasksLogic.js
@@ -1,26 +1,25 @@
 import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import SubmitForm from './SubmitForm/SubmitForm';
 import TasksList from './TasksList/TasksList';
 
 function TasksLogic() {
   const [todos, setTodos] = useState([
     {
-      id: 1,
+      id: uuidv4(),
       title: 'Setup development environment',
       completed: false,
     },
-    {
-      id: 2,
-      title: 'Develop website and add content',
-      completed: false,
-    },
-    {
-      id: 3,
-      title: 'Deploy to live server',
-      completed: false,
-    },
   ]);
-  const handleChange = (id) => {
+  const addTodoItem = (title) => {
+    const newTodo = {
+      id: uuidv4(),
+      title,
+      completed: false,
+    };
+    setTodos([...todos, newTodo]);
+  };
+  const handleCheckBox = (id) => {
     setTodos((prevState) => prevState.map((todo) => {
       if (todo.id === id) {
         return {
@@ -38,8 +37,8 @@ function TasksLogic() {
   };
   return (
     <div>
-      <SubmitForm />
-      <TasksList todosProps={todos} handleChange={handleChange} deleteTodo={deleteTodo} />
+      <SubmitForm addTodoItem={addTodoItem} />
+      <TasksList todosProps={todos} handleChange={handleCheckBox} deleteTodo={deleteTodo} />
     </div>
   );
 }
