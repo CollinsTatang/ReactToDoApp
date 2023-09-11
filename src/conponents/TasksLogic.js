a76024b7906ca9092247fb2afb8c8fa201233a3e
Collins Tatang <makungongcollins@gmail.com>
2023-09-11 14:13:23 +0100

    Add todos item to localStorage

--- src/conponents/TasksLogic.js
+++ src/conponents/TasksLogic.js
@@ -1,16 +1,21 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import SubmitForm from './SubmitForm/SubmitForm';
 import TasksList from './TasksList/TasksList';
 
 function TasksLogic() {
-  const [todos, setTodos] = useState([
-    {
-      id: uuidv4(),
-      title: 'Setup development environment',
-      completed: false,
-    },
-  ]);
+  // getting stored items
+  const getInitialTodos = () => {
+    const temp = localStorage.getItem('todos');
+    const savedTodos = JSON.parse(temp);
+    return savedTodos || [];
+  };
+  const [todos, setTodos] = useState(getInitialTodos());
+  useEffect(() => {
+    // storing todos items
+    const temp = JSON.stringify(todos);
+    localStorage.setItem('todos', temp);
+  }, [todos]);
   const addTodoItem = (title) => {
     const newTodo = {
       id: uuidv4(),
